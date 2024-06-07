f5355c975351057bcc762518f3da351163ae67c6
Shubham Kumar <shubham@glassfrog.design>
2024-06-07 12:53:14 +0530

    uodate

---
+++ src/components/techservice/tech.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const tech = () => {
+    const cardsData = [
+  {
+    title: 'Card Title 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    title: 'Card Title 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    title: 'Card Title 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+];
+  return (
+      <>
+       <section className="bg-white py-16 ">
+              <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+           <div className="flex justify-center py-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {cardsData.map((card, index) => (
+          <div key={index} className="card w-60 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 p-4 gap-4 rounded-lg shadow-md">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 mb-4">
+              <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+            </svg>
+            <div className="card__content">
+              <p className="card__title text-lg font-bold mb-2">{card.title}</p>
+              <p className="card__description">{card.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+                  </div>
+                  </div></section>
+      
+      </>
+  )
+}
+
+export default tech
