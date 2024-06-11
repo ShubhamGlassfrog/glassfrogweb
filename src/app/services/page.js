1683dcdfeec840868ace273e98263b11ff0170d6
Shubham Kumar <shubham@glassfrog.design>
2024-06-11 13:23:09 +0530

    link updated

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -54,8 +54,8 @@ We focus on delivering results that resonate with your specific brand and busine
                           <p className='text-white mb-6 flex items-center'>Our belief is to bring your visions to life. We combine technical expertise with digital marketing to craft engaging online experiences. Our result-driven approach is to design, develop, and optimize your website to drive traffic, engagement, and conversions. <br></br><br></br>
 We provide a full range of services including website design & development, advertising, SEO, content marketing, social media management, and graphic design, all customized to your needs. Our team prioritizes understanding your brand identity to ensure every aspect of your online presence resonates with your target audience. We work collaboratively to deliver exceptional results.
 </p>
-                            <p className="text-white mb-6 flex items-center"><a className='flex items-center gap-5' href='/services'>
-                               WEB SERVICES                          <svg width="160" height="14" viewBox="0 0 160 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M151.9 12.293L153.31 13.707L160 6.99997L153.31 0.292969L151.9 1.70697L156.182 5.99997H0V7.99997H156.182L151.9 12.293Z" fill="#ffffff"></path></svg> </a></p>
+                            <p className="text-white mb-6 flex items-center"><a className='flex items-center gap-5' href='/technology'>
+                               TECHNICAL SERVICES                          <svg width="160" height="14" viewBox="0 0 160 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M151.9 12.293L153.31 13.707L160 6.99997L153.31 0.292969L151.9 1.70697L156.182 5.99997H0V7.99997H156.182L151.9 12.293Z" fill="#ffffff"></path></svg> </a></p>
 
                       </div>
                        <div className="flex flex-col w-full md:w-1/2 sm:w-1/2">
@@ -167,7 +167,7 @@ We provide a full range of services including website design & development, adve
                              MARKETING
                           </h2>
                           <p className='text-white mb-6 flex items-center'>Glassfrog explores marketing and technical excellence. We are committed to both the creative and technical aspects of marketing. By incorporating AI, we offer advanced marketing solutions that wouldn't be possible with traditional methods. With AI we analyze vast amounts of data to gain deep insights into your target audience. We believe that personalized and relevant marketing messages lead to improved customer engagement and satisfaction.</p>
-                            <p className="text-white mb-6 flex items-center"><a className='flex items-center gap-5' href='/services'>
+                            <p className="text-white mb-6 flex items-center"><a className='flex items-center gap-5' href='/marketing'>
                                MARKETING SERVICES                          <svg width="160" height="14" viewBox="0 0 160 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M151.9 12.293L153.31 13.707L160 6.99997L153.31 0.292969L151.9 1.70697L156.182 5.99997H0V7.99997H156.182L151.9 12.293Z" fill="#ffffff"></path></svg> </a></p>
 
                       </div>
