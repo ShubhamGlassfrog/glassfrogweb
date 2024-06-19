47eb4224c7d174972eea7593cecab9f3780692e3
Shubham Kumar <shubham@glassfrog.design>
2024-06-19 11:26:37 +0530

    schema updated

--- src/app/digital-marketing/page.js
+++ src/app/digital-marketing/page.js
@@ -108,6 +108,33 @@ const page = () => {
         
         <script type="application/ld+json" dangerouslySetInnerHTML={{
           __html: JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Pay Per Click Ads",
+                "item": "https://www.glassfrogtech.com/pay-per-click-ads"
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Search Engine Optimisation",
+                "item": "https://www.glassfrogtech.com/search-engine-optimisation"
+              },
+              {
+                "@type": "ListItem",
+                "position": 3,
+                "name": "Social Media Advertising",
+                "item": "https://www.glassfrogtech.com/social-media-advertising"
+              }
+            ]
+          })
+        }} />
+        
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
             "@context": "https://schema.org",
             "@type": "ItemList",
             "itemListElement": [
