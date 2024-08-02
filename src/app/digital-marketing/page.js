db27d76be3597a98d20e1da63b5e3d818b6d0ac2
Shubham Kumar <shubham@glassfrog.design>
2024-08-02 14:23:03 +0530

    faq schema updated

--- src/app/digital-marketing/page.js
+++ src/app/digital-marketing/page.js
@@ -132,6 +132,51 @@ const page = () => {
             ]
           })
         }} />
+          <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `
+            {
+              "@context": "https://schema.org",
+              "@type": "FAQPage",
+              "mainEntity": [
+                {
+                  "@type": "Question",
+                  "name": "What are the top services offered by digital marketing agencies in cities like London, and others?",
+                  "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "There are a variety of marketing services offered by digital marketing agencies in cities such as London, and others. Marketers often seek services such as Lead Generation, Pay Per Click Campaigns, Search Engine Optimization, and Social Network Advertising. These digital marketing services help businesses to increase their online engagement and outreach."
+                  }
+                },
+                {
+                  "@type": "Question",
+                  "name": "How can a Ads Agency help me boost my business through digital marketing?",
+                  "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "A good ads agency would offer social media advertising, SEO & Marketing, and Google PPC Ads. These techniques in the domain of digital marketing will help businesses to approach the users online, based on the product audience and niche, through a customised and specific plan. Approaching the right audience on the right platform is a skill that an ads agency specialises in. Therefore, they shall help boosting business by running campaigns on digital platforms."
+                  }
+                },
+                {
+                  "@type": "Question",
+                  "name": "What are the methods for good lead generation campaign that a digital marketing agency uses?",
+                  "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "A customised and personalised social media advertising campaign fuels a good lead generation and conversion rate. Selecting the exact type of audience and estimating the best set of users for products and services will help filter out the consumers who are highly interested in purchase. This shall hike the quantity as well as the quantity of the leads. A skilled and experienced digital marketing agency shall be the best resource to refer to in order to create a good lead generation campaign."
+                  }
+                },
+                {
+                  "@type": "Question",
+                  "name": "What are the features of the best search engine optimization agency for digital marketing of my business?",
+                  "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "A good SEO & Marketing agency would employ the best methods for Search Engine Optimisation for your business. These methods will be widely distributed throughout arenas such as on page SEO, off page SEO, backlinks creation, and much more. An SEO agency would plan to grow the business and bring hike in organic traffic within certain intervals of time. Therefore, the best search engine optimization agency would strategically engage in the digital marketing of your business."
+                  }
+                }
+              ]
+            }
+          `,
+        }}
+      />
         
        
        
